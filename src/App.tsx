69a9af041a1c3c38fed547c3bcc50e60c1761f08
GabrielRaiolR <gabrielraiolr@gmail.com>
2026-07-18 19:28:12 -0300

    feat: add lucide-react dependency and enhance App component layout
    Introduced the lucide-react library for icon usage. Updated the App component to include new components (Logo, Menu, CountDown) and added a form for task input. Enhanced global styles for better form layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+
 import './styles/theme.css';
 import './styles/global.css';
-
-import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label id='input' htmlFor=''>
+              Task
+            </label>
+            <input id='input' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Total time: 00:00</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 </p>
+          </div>
+
+          <div className='formRow'>
+            <button type='submit'>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
